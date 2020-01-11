17acd6ad8efddb7ea87e47cf92d1e518d981c997
Sonata Arrington <sarrington1009@gmail.com>
2020-01-11 21:24:46 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -109,9 +109,10 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      
-     alans_new_info = programmer_hash[:alan_kay][:known_for]
+     oo = programmer_hash[:alan_kay][:known_for]
      alans_new_info = "GUI"
-     alans_new_info
+     alans_new_info = oo
+     oo
 end
 
 changing_alan
