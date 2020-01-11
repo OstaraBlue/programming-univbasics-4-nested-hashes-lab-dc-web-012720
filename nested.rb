9eccb93c90f7eb225e92ee6f0fe7897347ad141b
Sonata Arrington <sarrington1009@gmail.com>
2020-01-11 18:03:36 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -67,13 +67,10 @@ dennis_ritchies_language
 
 def adding_matz
 # add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
 
 	programmer_hash = 
  		{
+ 		  
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
